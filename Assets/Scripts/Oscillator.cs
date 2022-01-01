875e5256e1683b4d09b92fcc435bd68c6c84e40d
Void7 <limen191100@gmail.com>
2022-01-01 20:06:44 +0300

    Update Oscillator.cs
    Now Oscillator works on time.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period;
+    float movementFactor;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,11 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // Growing over time
+        const float tau = Mathf.PI * 2;      // Get tau
+        float sinWay = Mathf.Sin(cycles * tau); // Get result of sin between -1 and 1
+        movementFactor = (sinWay + 1f) / 2f; // make -1 and 1 to 0 and 1
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
